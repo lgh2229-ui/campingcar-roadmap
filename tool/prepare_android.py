2b0a233bae11a2a96086814bff53780a58287ba0
lgh2229-ui <lgh2229@gmail.com>
2026-09-25 08:42:34 +0900

    Fix desugaring Gradle newline generation

--- tool/prepare_android.py
+++ tool/prepare_android.py
@@ -53,13 +53,13 @@ for gradle_name in ['android/app/build.gradle.kts', 'android/app/build.gradle']:
         if compile_marker in g and 'isCoreLibraryDesugaringEnabled = true' not in g:
             g = g.replace(compile_marker, compile_marker + '        isCoreLibraryDesugaringEnabled = true\\n', 1)
         if 'coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:' not in g:
-            g += '\\ndependencies {\\n    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")\\n}\\n'
+            g += '\ndependencies {\n    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")\n}\n'
     else:
         compile_marker = '    compileOptions {\\n'
         if compile_marker in g and 'coreLibraryDesugaringEnabled true' not in g:
             g = g.replace(compile_marker, compile_marker + '        coreLibraryDesugaringEnabled true\\n', 1)
         if "coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:" not in g:
-            g += "\\ndependencies {\\n    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.5'\\n}\\n"
+            g += "\ndependencies {\n    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.5'\n}\n"
     g = g.replace('applicationId = "kr.co.campingcarroadmap.campingcar_roadmap"', 'applicationId = "kr.co.campingcarroadmap.app"')
     g = g.replace("applicationId 'kr.co.campingcarroadmap.campingcar_roadmap'", "applicationId 'kr.co.campingcarroadmap.app'")
     g = g.replace('applicationId "kr.co.campingcarroadmap.campingcar_roadmap"', 'applicationId "kr.co.campingcarroadmap.app"')
